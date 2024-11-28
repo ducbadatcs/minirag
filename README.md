b1014533d445f249e674783b1b15607ac879ea80
Hoàng Minh Đức <71325886+ducbadatcs@users.noreply.github.com>
2024-11-28 17:38:34 +0700

    added default functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Application to summarize URL of a website using LangChain AI.
--- summarize.py
+++ summarize.py
@@ -1,6 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.retrievers import WikipediaRetriever
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+
 import os
 os.environ["GOOGLE_API_KEY"] = "AIzaSyBmD7z5uWUA5lCGZCvyDhL9bqgXpziZXGY"
+
+import requests
+
+from flask import Flask, render_template, redirect, url_for, request
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-pro",
@@ -11,5 +19,20 @@ llm = ChatGoogleGenerativeAI(
     # other params...
 )
 
-def pull_website(url: str) -> str:
-    return ""
+retriever = WikipediaRetriever()
+
+
+def summarize_wiki(query: str) -> str:
+    results: list[str] = [i.page_content for i in retriever.invoke(query)]
+    prompt_list = [
+		("system", "You are a helpful assistant"),
+		("user", "Can you help me summarize information about {query}? Based on the info?")
+	]
+    
+    for info in results:
+        prompt_list.append(
+			("user", info)
+		)
+        
+    prompt = ChatPromptTemplate.from_messages(prompt_list).invoke({"{query}", query})
+    return llm.invoke(prompt).content
